6bc186aacb91ba2ef0c776a48d1362a74e939d3f
Masmaskernel <154093091+Masmaskernel@users.noreply.github.com>
2024-01-26 05:45:56 +0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update
+apt install apache2 isc-dhcp-server -y
+mv interfaces /etc/network/interfaces
+cp -r satria /var/wwww/satria
+cp -r fresto /var/www/fresto
+cp sites-available/fresto.conf /etc/apache2/sites-available/fresto.conf
+cp sites-available/satria.conf /etc/apache2/sites-available/satria.conf
+sudo su
+a2ensite fresto.conf
+a2ensite satria.conf
+systemctl reload apache2
+exit
+cp dhcp/dhcp.conf /etc/dhcp/dhcpd.conf
+systemctl restart networking
